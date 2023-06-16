a6bd5bd76e47e1794f970c53a38a1454051d2207
Elissa-DI <elissadusabe@gmail.com>
2023-06-16 17:09:06 +0200

    new changes on front

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
 import './App.css'
 import { useState, useRef, useLayoutEffect, useMemo, useCallback } from 'react';
 import Axios from 'axios'
 import { MovieCard } from './components/MovieCard';
-import  YouTube  from 'react-youtube';
-import { AiOutlineSearch } from 'react-icons/ai';
-import {FaCopyright} from "react-icons/fa"
+import YouTube from 'react-youtube';
+import { FaCopyright } from "react-icons/fa"
 
 function App() {
-  const imageUrl="https://image.tmdb.org/t/p/w1280"
-  const[movies,setMovies]=useState([])
-  const[selectedMovie,setSelectedMovie]=useState({})
-  const [playTrailer,setPlayTrailer]=useState(false)
-  const [notFound,setNotFound]=useState(false)
-  const apiUrl="https://api.themoviedb.org/3/";
-  const apiKey="253eb6a5f22792e1412b47d42d247239"
+  const imageUrl = "https://image.tmdb.org/t/p/w1280"
+  const [movies, setMovies] = useState([])
+  const [selectedMovie, setSelectedMovie] = useState({})
+  const [playTrailer, setPlayTrailer] = useState(false)
+  const [notFound, setNotFound] = useState(false)
+  const apiUrl = "https://api.themoviedb.org/3/";
+  const apiKey = "253eb6a5f22792e1412b47d42d247239"
   const fetchMovies = async (searchKey) => {
     const type = searchKey ? "search" : "discover";
     try {
@@ -29,7 +30,7 @@ function App() {
       console.error(error);
     }
   };
-  
+
   const fetchMovie = async (id) => {
     try {
       const { data } = await Axios.get(`${apiUrl}/movie/${id}`, {
@@ -43,8 +44,8 @@ function App() {
       console.error(error);
     }
   };
-  const selectMovie=useCallback(async(movie)=>{
-    if(!movie){
+  const selectMovie = useCallback(async (movie) => {
+    if (!movie) {
       setNotFound(true)
       return;
     }
@@ -59,165 +60,146 @@ function App() {
       setPlayTrailer(false);
       setSelectedMovie(movie);
     }
-  },[movies])
-  
-  
+  }, [movies])
 
-const renderTrailer = () => {
-  try {
-    if (selectedMovie && selectedMovie.videos && selectedMovie.videos.results && selectedMovie.videos.results.length > 0) {
-      const trailer = selectedMovie.videos.results.find((vid) => vid.name === "Official Trailer");
-      const key = trailer ? trailer.key : selectedMovie.videos.results[0].key;
-      return (
-        <YouTube
-          videoId={key}
-          className='youtubeContainer sm:w-20'
-         
-          opts={{
-            // width: "120px",
-            playerVars: {
-              autoplay: true,
-            },
-          }}
-        />
-      );
-    }
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-  useLayoutEffect(()=>{
-      fetchMovies()
-  },[])
-  
-  const renderMovies=()=>{    
-      return movies.map((movie)=>{
-          return <MovieCard
-          key={movie.id}
-          movie={movie}
-          selectMovie={selectMovie}
+  const renderTrailer = () => {
+    try {
+      if (selectedMovie && selectedMovie.videos && selectedMovie.videos.results && selectedMovie.videos.results.length > 0) {
+        const trailer = selectedMovie.videos.results.find((vid) => vid.name === "Official Trailer");
+        const key = trailer ? trailer.key : selectedMovie.videos.results[0].key;
+        return (
+          <YouTube
+            videoId={key}
+            className='youtubeContainer sm:w-20'
+            opts={{
+              playerVars: {
+                autoplay: true,
+              },
+            }}
           />
-      })
+        );
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useLayoutEffect(() => {
+    fetchMovies()
+  }, [])
+
+  const renderMovies = () => {
+    return movies.map((movie) => {
+      return <MovieCard
+        key={movie.id}
+        movie={movie}
+        selectMovie={selectMovie}
+      />
+    })
   }
-  const MemoMovies=useMemo(()=>{
+  const MemoMovies = useMemo(() => {
     return renderMovies
-  },[movies])
-// console.log(movies[0].original_title);
-  const [searchKey,setSearchKey]=useState("")
+  }, [movies])
+  // console.log(movies[0].original_title);
+  const [searchKey, setSearchKey] = useState("")
   const searching = (e) => {
     setNotFound(false)
     const targetValue = e.target.value;
     if (targetValue.includes(" ")) {
-     
       setSearchKey(targetValue);
     } else {
       setSearchKey(targetValue);
     }
-   
-
-
   };
-  
-  const searchMovie=(e)=>{
-      e.preventDefault()
-      console.log(searchKey);
-      fetchMovies(searchKey)
+
+  const searchMovie = (e) => {
+    e.preventDefault()
+    console.log(searchKey);
+    fetchMovies(searchKey)
   }
-  
 
-    const inputRef=useRef(null)
-    const handleRef=()=>{
-      const value=inputRef.current.value;
-      if(value===""){
-        inputRef.current.focus()
-      }else{
-        inputRef.current.value=""
-      }
+  const inputRef = useRef(null)
+  const handleRef = () => {
+    const value = inputRef.current.value;
+    if (value === "") {
+      inputRef.current.focus()
+    } else {
+      inputRef.current.value = ""
     }
-  
+  }
 
-return(
-<div className={`grid grid-row-3 h-full `} >
-   <header className='header grid grid-cols-2 gap-10 m-0'>
-      
-       
-      <h1 className="title">TrailMix</h1>
-     
-    
-      <form onSubmit={searchMovie} className="flex justify-center items-center">
-        <input type="text" name="searchInput" id="seachInput" onChange={searching} className="search" ref={inputRef} placeholder='search trailer....' />
-        {/* <button type="submit" className=" searchButton" onClick={handleRef}><AiOutlineSearch size={25} className=''/></button> */}
-      </form>
-
-      
-   </header>
-   <section className={`h-[80vh] `}>
-    
-     <div style={{background:`url(${imageUrl}${selectedMovie.backdrop_path}) `}} className='h-full cover'>
-       <div className='all'>
-          {selectedMovie.videos && playTrailer ? (
-          <>
-           <div className='' >
-             { renderTrailer()}
-            </div>
-            <div className='' >
-               <h1 className='text-5xl'>{selectedMovie.title}</h1>
+  return (
+    <div className={`grid grid-row-3 h-full `} >
+      <header className='header grid grid-cols-2 gap-10 py-1 px-10'>
+        <div className='flex gap-2'>
+          <img src="/img/logo.png" alt="Logo" className='w-10 h-12 mt-3'/>
+          <h1 className="title hidden md:block">TrailMix</h1>
+        </div>
+        <form onSubmit={searchMovie} className="flex justify-end items-center">
+          <input type="text" name="searchInput" id="seachInput" onChange={searching} className="search" ref={inputRef} placeholder='search trailer....' />
+          {/* <button type="submit" className=" searchButton" onClick={handleRef}><AiOutlineSearch size={25} className=''/></button> */}
+        </form>
+      </header>
+      <section className={`h-[80vh] `}>
+        <div style={{ background: `url(${imageUrl}${selectedMovie.backdrop_path}) ` }} className='h-full cover'>
+          <div className='all'>
+            {selectedMovie.videos && playTrailer ? (
+              <>
+                <div className='' >
+                  {renderTrailer()}
+                </div>
+                <div className='' >
+                  <h1 className='text-5xl'>{selectedMovie.title}</h1>
                   {selectedMovie.overview ? <p className='my-2 text-xl'>{selectedMovie.overview}</p> : null}
-            </div>
-            <div className=''>
-               {playTrailer && (
-                <button className='bg-blue-900 hover:bg-blue-950  font-bold py-2 px-4 border border-blue-900 rounded my-3' onClick={() => {setPlayTrailer(false)}}>
-                    Close
-                </button>
-               )}
-            </div>
-          </>
-         )    : (
+                </div>
+                <div className=''>
+                  {playTrailer && (
+                    <button className='bg-blue-900 hover:bg-blue-950  font-bold py-2 px-4 border border-blue-900 rounded my-3' onClick={() => { setPlayTrailer(false) }}>
+                      Close
+                    </button>
+                  )}
+                </div>
+              </>
+            ) : (
 
-    <div className=''>
-    <h1 className='text-5xl p-2'>{selectedMovie.title}</h1>
-    {selectedMovie.overview ? <p className='my-2 text-xl'>{selectedMovie.overview}</p> : null}
-      {!playTrailer && (
-    <button onClick={() => setPlayTrailer(true)} className='bg-blue-900 hover:bg-blue-950  font-bold py-2 my-3 px-4 border border-blue-900 rounded '>
-    Play Trailer
-  </button>
-  )}
-</div>
-
-  )}
-       </div>
-      
-      </div>
-   </section>
-   <main>
-   <div className='grid grid-cols-3 gap-3 bg-black movie ' onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
-   {movies ?
-          MemoMovies( )
-      :
-      
-      <div className='bg-blue-900'></div>}  
-   </div>
-   </main>
-   <footer className='bg-black'>
-   {notFound && <p className='title'>no movie found with that name</p>}
-   <p className='items-center flex justify-center'><FaCopyright color='#fff' size={30}/><span className='title'>jodos</span></p>
-   </footer>
-
-</div>    
-)
-
+              <div className=''>
+                <h1 className='text-5xl p-2'>{selectedMovie.title}</h1>
+                {selectedMovie.overview ? <p className='my-2 text-xl'>{selectedMovie.overview}</p> : null}
+                {!playTrailer && (
+                  <button onClick={() => setPlayTrailer(true)} className='bg-blue-900 hover:bg-blue-950  font-bold py-2 my-3 px-4 border border-blue-900 rounded '>
+                    Play Trailer
+                  </button>
+                )}
+              </div>
+            )}
+          </div>
+        </div>
+      </section>
+      <main>
+        <div className='grid grid-cols-3  gap-3 movie' onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
+          {
+            movies ? MemoMovies() :
+              <div className='shadow-md'></div>
+          }
+        </div>
+      </main>
+      <footer className='bg-black'>
+        {notFound && <p className='title'>no movie found with that name</p>}
+        <p className='items-center flex justify-center'><FaCopyright color='#fff' size={30} /><span className='text-white'>Elissa</span></p>
+      </footer>
+    </div>
+  )
 }
-export default App
+export default App;
 
 
 
-        
-         
-          
 
 
-   
-    
-  
-    
+
+
+
+
+
+
+
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,19 +1,37 @@
-export const MovieCard=({movie,selectMovie})=>{
+/* eslint-disable react/prop-types */
+import { FaPlay } from "react-icons/fa"
+
+export const MovieCard = ({ movie, selectMovie }) => {
   //props.movie;
   //props.selectMovie;
 
-  const moviUrl="https://image.tmdb.org/t/p/w500"
-  
-  return(
-    <div className="bg-red p-3 text-white text-xl rounded-lg other m-3" onClick={() => {selectMovie(movie)}}> 
-    {movie.poster_path ? <img src={`${moviUrl}${movie.poster_path}`} alt="cover image" className="rounded-xl"/>
-      :
-    <div>no image found</div>
-    }
-    <h5 className="flex justify-center items-center m-2"> {movie.title}</h5>
-  </div>
-  
-  
-  
+  const moviUrl = "https://image.tmdb.org/t/p/w500"
+
+  return (
+    <div className="relative bg-red text-white text-xl rounded-lg other m-3">
+      {
+        movie.poster_path ?
+          <>
+            <img src={`${moviUrl}${movie.poster_path}`} alt="cover-image" className="rounded-xl" />
+            <div className="absolute top-0 w-full h-full flex flex-col justify-center items-center gap-5 bg-blue-300 rounded-lg opacity-0 hover:opacity-20">
+              <h5 className="flex justify-center items-center m-2 font-bold opacity-80 z-50">{movie.title}</h5>
+              <button
+                onClick={() => { selectMovie(movie) }}
+                className="animate-pulse shadow-lg py-2 px-5 rounded-full bg-blue-950 h-14"
+              >
+                <FaPlay size={18} />
+              </button>
+            </div>
+          </>
+          :
+          <div>
+            no image found
+          </div>
+      }
+      <h5 className="flex justify-center items-center m-2 font-semibold"> {movie.title}</h5>
+    </div>
+
+
+
   )
 }
